1277ee5e283bf38e04f743fd4adf4046cd29c727
Aysel9 <89453868+Aysel9@users.noreply.github.com>
2021-11-24 17:12:10 +0400

    Add files via upload

---
+++ js/packages/web/src/components/Section2/index.tsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+import { useWallet } from '@solana/wallet-adapter-react';
+import {
+  Cog,
+  CurrentUserBadge,
+  CurrentUserBadgeMobile,
+} from '../CurrentUserBadge';
+import { Link } from 'react-router-dom';
+import { Button, Menu, Modal } from 'antd';
+import { Notifications } from '../Notifications';
+import useWindowDimensions from '../../utils/layout';
+import { MenuOutlined } from '@ant-design/icons';
+import { HowToBuyModal } from '../HowToBuyModal';
+import { ConnectButton } from '@oyster/common';
+
+
+
+
+export const Section2 = () => {
+  const { connected } = useWallet();
+  return (
+    <>
+     <div className="Section2" style={{display: connected ? 'none' : 'flex'}}>
+     <h1>HI I AM Section2</h1>
+     </div>
+    </>
+  );
+};
+
+
+
+
+
+
